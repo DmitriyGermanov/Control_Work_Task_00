29eb2d9f0de3cb913126110b6e6c0cfe3e071ea5
Dmitriy Germanov <seltis.gtn@gmail.com>
2024-01-20 21:13:29 +0300

    readme update

--- model/file_handler.py
+++ model/file_handler.py
@@ -6,9 +6,22 @@ class FileHandler:
         self.file_path = os.path.dirname(os.path.realpath(__file__)) + "\\data\\" + filename
 
     def read_file(self):
-        with open(self.file_path, "r") as f:
-            return f.read()
+        flag = True
+        while flag:
+            try:
+                with open(self.file_path, "r") as f:
+                    flag = False
+                    return f.read()
+            except FileNotFoundError:
+                try:
+                    with open(self.file_path, "w") as f:
+                        f.write("")
+                except FileNotFoundError:
+                    pass
 
     def write_file(self, content):
-        with open(self.file_path, "w") as f:
-            f.write(content)
+        try:
+            with open(self.file_path, "w") as f:
+                f.write(content)
+        except FileNotFoundError:
+            pass
--- view/console_ui.py
+++ view/console_ui.py
@@ -26,7 +26,10 @@ class ConsoleUI:
             self.execute()
 
     def execute(self):
-        self.menu.execute(int(input()))
+        try:
+            self.menu.execute(int(input()))
+        except ValueError:
+            print("Ошибка! Введите число от 1 до " + str(len(self.menu.command_list)) + "\n")
 
     def add_note(self):
         print("Пожалуйста, введите заголовок новой заметки:")
@@ -48,33 +51,57 @@ class ConsoleUI:
         print()
 
     def show_note_by_index(self):
-        note = self.presenter.show_note_by_index(int(input(f"Введите номер заметки: от 1 до "
-                                                           f"{len(self.presenter.show_all_notes())}\n")) - 1)
-        print(f"{note.__str__()}\n")
+        flag = True
+        if len(self.presenter.show_all_notes()) == 0:
+            print("Заметки отсутствуют\n")
+        else:
+            while flag:
+                try:
+                    note = self.presenter.show_note_by_index(int(input(f"Введите номер заметки: от 1 до "
+                                                                       f"{len(self.presenter.show_all_notes())}\n")) - 1)
+                    flag = False
+                    print(f"{note.__str__()}\n")
+                except ValueError:
+                    print(f"Ошибка! Ввод должен быть числом\n")
 
     def update_note_by_index(self):
-        index = int(input(f"Введите номер заметки: от 1 до "
-                          f"{len(self.presenter.show_all_notes())}\n")) - 1
-        print("Введите заголовок заметки:")
-        title = input()
-        print("Введите текст заметки:")
-        text = input()
-        if self.presenter.update_note(index, title, text):
-            print("Заметка успешно обновлена\n")
+        if len(self.presenter.show_all_notes()) == 0:
+            print("Заметки отсутствуют\n")
         else:
-            print("Ошибка. Заметка не обновлена\n")
+            flag = True
+            while flag:
+                try:
+                    index = int(input(f"Введите номер заметки: от 1 до "
+                                      f"{len(self.presenter.show_all_notes())}\n")) - 1
+                    print("Введите заголовок заметки:")
+                    title = input()
+                    print("Введите текст заметки:")
+                    text = input()
+                    if self.presenter.update_note(index, title, text):
+                        print("Заметка успешно обновлена\n")
+                        flag = False
+                    else:
+                        print("Ошибка. Заметка не обновлена\n")
+                except ValueError:
+                    print(f"Ошибка! Ввод должен быть числом\n")
 
     def show_notes_by_date(self):
-        start_date = date.fromisoformat(input("Введите начальную дату в "
-                                              "формате YYYY-MM-DD: "))
-        end_date = date.fromisoformat(input("Введите конечную дату в "
-                                            "формате YYYY-MM-DD: "))
-        notes_string = self.presenter.show_notes_by_date(start_date, end_date)
-        if notes_string == "":
-            print("Заметки не найдены\n")
-        else:
-            print(f"Список заметок в интервале: {start_date.__str__()} до {end_date.__str__()}\n")
-            print(notes_string)
+        flag = True
+        while flag:
+            try:
+                start_date = date.fromisoformat(input("Введите начальную дату в "
+                                                      "формате YYYY-MM-DD: "))
+                end_date = date.fromisoformat(input("Введите конечную дату в "
+                                                    "формате YYYY-MM-DD: "))
+                notes_string = self.presenter.show_notes_by_date(start_date, end_date)
+                if notes_string == "":
+                    print("Заметки не найдены\n")
+                else:
+                    print(f"Список заметок в интервале: {start_date.__str__()} до {end_date.__str__()}\n")
+                    print(notes_string)
+                flag = False
+            except ValueError:
+                print(f"Ошибка! Ввод должен быть датой\n")
 
     def show_notes_by_title(self):
         print("Введите заголовок заметки:")
@@ -87,9 +114,18 @@ class ConsoleUI:
             print(notes_string)
 
     def delete_note_by_index(self):
-        index = int(input(f"Введите номер заметки: от 1 до "
-                          f"{len(self.presenter.show_all_notes())}\n")) - 1
-        if self.presenter.delete_note(index):
-            print("Заметка успешно удалена\n")
+        if len(self.presenter.show_all_notes()) == 0:
+            print("Заметки отсутствуют\n")
         else:
-            print("Ошибка. Заметка не удалена\n")
+            flag = True
+            while flag:
+                try:
+                    index = int(input(f"Введите номер заметки: от 1 до "
+                                      f"{len(self.presenter.show_all_notes())}\n")) - 1
+                    if self.presenter.delete_note(index):
+                        print("Заметка успешно удалена\n")
+                    else:
+                        print("Ошибка. Заметка не удалена\n")
+                    flag = False
+                except ValueError:
+                    print(f"Ошибка! Ввод должен быть числом\n")
